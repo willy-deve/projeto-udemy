784723f29d9502681b2741b8f85f746ecd8365ee
willy <willydasilva29@gmail.com>
2022-11-15 12:00:25 -0300

    estilizando header e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import Footer from './componentes/Footer'
-import Header from './componentes/Header'
+import Footer from './componentes/Footer/Footer'
+import Header from './componentes/Header/Header'
+import styles from './App.module.css'
+import Form from './componentes/Form/Form'
+import List from './componentes/List/List'
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Conteudo</h1>
+      <main className={styles.main}>
+        <h1>Conteudo</h1>
+      </main>
       <Footer />
     </div>
   )
--- src/componentes/Footer/Footer.tsx
+++ src/componentes/Footer/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './Footer.module.css'
 
 const Footer: React.FC = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>
-        <span>React + TS Todo</span>
+        <span>React + TS Todo</span> @ 2022
       </p>
     </footer>
   )
