a1ebaf71185446fcc654faefe792618260c68bfb
willy <willydasilva29@gmail.com>
2022-11-15 11:19:53 -0300

    criado o header e footer sem estilização

---
+++ src/componentes/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>
+        <span>React + TS Todo</span>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/componentes/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>React + TS Todo</h1>
+    </header>
+  )
+}
+
+export default Header
